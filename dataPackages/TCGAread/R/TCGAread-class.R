f0998dc1610bd217a0d601a61272154f47769f59
LisaMc <lmcferri@fredhutch.org>
2015-09-21 19:58:10 -0700

    TCGAread

---
+++ dataPackages/TCGAread/R/TCGAread-class.R
@@ -0,0 +1,27 @@
+#----------------------------------------------------------------------------------------------------
+printf = function (...) print (noquote (sprintf (...)))
+#----------------------------------------------------------------------------------------------------
+.TCGAread <- setClass ("TCGAreadClass", contains = "SttrDataPackageClass")
+#----------------------------------------------------------------------------------------------------
+#setGeneric('historyTable',   signature='obj', function (obj) standardGeneric ('historyTable'))
+#----------------------------------------------------------------------------------------------------
+# constructor
+#TCGAread <- function(name="", matrices=list(), history=PatientHistory(), manifest=data.frame())
+TCGAread <- function()
+{
+  dir <- system.file(package="TCGAread", "extdata")
+  stopifnot(file.exists(dir))
+  data <- SttrDataPackage:::.loadFiles(dir)
+
+  obj <- .TCGAread(SttrDataPackage(name="TCGAread",
+                                  matrices=data$matrices,
+                                  data.frames=data$data.frames,
+                                  history=data$history,
+                                  manifest=data$manifest,
+                                  genesets=data$genesets))
+
+  obj
+
+} # TCGAread constructor
+
+#----------------------------------------------------------------------------------------------------
