c92dbfd8fca17090f96fa55bf08e8c56ccfd3660
LisaMc <lmcferri@fredhutch.org>
2015-09-21 19:56:26 -0700

    TCGAlusc

---
+++ dataPackages/TCGAlusc/R/TCGAlusc-class.R
@@ -0,0 +1,26 @@
+#----------------------------------------------------------------------------------------------------
+printf = function (...) print (noquote (sprintf (...)))
+#----------------------------------------------------------------------------------------------------
+.TCGAlusc <- setClass ("TCGAluscClass", contains = "SttrDataPackageClass")
+#----------------------------------------------------------------------------------------------------
+#setGeneric('historyTable',   signature='obj', function (obj) standardGeneric ('historyTable'))
+#----------------------------------------------------------------------------------------------------
+# constructor
+#TCGAlusc <- function(name="", matrices=list(), history=PatientHistory(), manifest=data.frame())
+TCGAlusc <- function()
+{
+  dir <- system.file(package="TCGAlusc", "extdata")
+  stopifnot(file.exists(dir))
+  data <- SttrDataPackage:::.loadFiles(dir)
+
+  obj <- .TCGAlusc(SttrDataPackage(name="TCGAlusc",
+                                 matrices=data$matrices,
+                                 data.frames=data$data.frames,
+                                 history=data$history,
+                                 manifest=data$manifest))
+
+  obj
+
+} # TCGAlusc constructor
+
+#----------------------------------------------------------------------------------------------------
