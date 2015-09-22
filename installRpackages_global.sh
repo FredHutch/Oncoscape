754a2390bd3ba3703e9303c55fdd973175455c92
LisaMc <lmcferri@fredhutch.org>
2015-09-21 19:11:48 -0700

    added accessory files

---
+++ installRpackages_global.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# run with . ./setupLocalR.sh to prevent forking a subshell
+Rscript installBioconductorPackages.R
+
+cd analysisPackages
+R  --vanilla CMD INSTALL --no-test-load --no-lock PCA
+R  --vanilla CMD INSTALL --no-test-load --no-lock PLSR
+ 
+cd ../dataPackages
+R  --vanilla CMD INSTALL --no-test-load --no-lock PatientHistory
+R  --vanilla CMD INSTALL --no-test-load --no-lock SttrDataPackage
+R  --vanilla CMD INSTALL --no-test-load --no-lock DEMOdz
+R  --vanilla CMD INSTALL --no-test-load --no-lock TCGAgbm
+R  --vanilla CMD INSTALL --no-test-load --no-lock TCGAlgg
+R  --vanilla CMD INSTALL --no-test-load --no-lock TCGAbrain
+R  --vanilla CMD INSTALL --no-test-load --no-lock DGI
+
+cd ../oncoDev14/Oncoscape
+R  --vanilla CMD INSTALL --no-test-load --no-lock .
+
---
+++ installRpackages_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# run with . ./setupLocalR.sh to prevent forking a subshell
+Rscript installBioconductorPackages.R
+
+cd analysisPackages
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock PCA
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock PLSR
+ 
+cd ../dataPackages
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock PatientHistory
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock SttrDataPackage
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock DEMOdz
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock TCGAgbm
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock TCGAlgg
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock TCGAbrain
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock DGI
+
+cd ../Oncoscape
+R  --vanilla CMD INSTALL -l $R_LIBS --no-test-load --no-lock .
+
