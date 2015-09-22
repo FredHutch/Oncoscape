52dd299f783c4f87c2600efafe18b25130c7d16a
LisaMc <lmcferri@fredhutch.org>
2015-09-21 19:57:23 -0700

    TCGAprad

---
+++ dataPackages/TCGAprad/R/TCGAprad-class.R
@@ -0,0 +1,26 @@
+#----------------------------------------------------------------------------------------------------
+printf = function (...) print (noquote (sprintf (...)))
+#----------------------------------------------------------------------------------------------------
+.TCGAprad <- setClass ("TCGApradClass", contains = "SttrDataPackageClass")
+#----------------------------------------------------------------------------------------------------
+#setGeneric('historyTable',   signature='obj', function (obj) standardGeneric ('historyTable'))
+#----------------------------------------------------------------------------------------------------
+# constructor
+#TCGAprad <- function(name="", matrices=list(), history=PatientHistory(), manifest=data.frame())
+TCGAprad <- function()
+{
+  dir <- system.file(package="TCGAprad", "extdata")
+  stopifnot(file.exists(dir))
+  data <- SttrDataPackage:::.loadFiles(dir)
+
+  obj <- .TCGAprad(SttrDataPackage(name="TCGAprad",
+                                 matrices=data$matrices,
+                                 data.frames=data$data.frames,
+                                 history=data$history,
+                                 manifest=data$manifest))
+
+  obj
+
+} # TCGAprad constructor
+
+#----------------------------------------------------------------------------------------------------
