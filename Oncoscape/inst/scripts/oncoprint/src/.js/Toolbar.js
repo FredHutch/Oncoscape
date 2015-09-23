0b005f4590f5be843c1ab4f580a382ed0a88a633
Jenny Zhang <canaantt@gmail.com>
2015-09-23 15:15:20 -0700

    add oncoprint

---
+++ Oncoscape/inst/scripts/oncoprint/src/.js/Toolbar.js
@@ -0,0 +1,16 @@
+var toolbar_events = {
+	
+};
+
+function Toolbar() {
+	var self = this;
+	
+	self.bindEvents = function(oncoprint) {
+	}
+}
+
+function ToolbarRenderer() {
+
+}
+
+module.exports = Toolbar;
