852f25e1805ddbd4ba46b1feb57ec0dd101a3141
pshannon-bioc <pshannon@fhcrc.org>
2015-10-14 12:47:35 -0700

    not previously archived

---
+++ Oncoscape/inst/scripts/markersAndSamples/runMarkers.R
@@ -0,0 +1,13 @@
+library(OncoDev14)
+scriptDir <- "markersAndSamples"
+userID <- "autotest@nowhere.org"
+#current.datasets <- "DEMOdz;UWlung"
+#current.datasets <- "DEMOdz"
+#current.datasets <- "DEMOdz;DEMOdz;DEMOdz"
+#current.datasets <- "DEMOdz;TCGAgbm"
+current.datasets <- "DEMOdz;TCGAgbm;TCGAbrain"
+port <- 7575
+onco <- OncoDev14(port=port, scriptDir=scriptDir, userID=userID, datasetNames=current.datasets)
+browseURL(sprintf("http://localhost:%d", port))
+if(Sys.info()[["nodename"]] != "lopez") 
+   run(onco)
