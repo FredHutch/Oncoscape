939a2bf911911857c48ff53451db0fcbb07d4108
pshannon-bioc <pshannon@fhcrc.org>
2015-10-27 14:00:18 -0700

    datasets payload field in getUserInfo response now is parsed into an array of strings

--- Oncoscape/R/getUserInfo.R
+++ Oncoscape/R/getUserInfo.R
@@ -5,8 +5,12 @@ getUserInfo <- function(ws, msg)
   printf("getUserInfo R message handler called");
   print(msg);
   printf("  state fields: %s", paste(ls(state), collapse=", "))
+  
+  datasetNames <- strsplit(state$rawDatasetNames, ";")[[1]];
+  
   payload <- list(userID=state$userID,
-                  datasets=state$rawDatasetNames)
+                  datasets=datasetNames)
+                  #datasets=state$rawDatasetNames)
   
   return.msg <- list(cmd=msg$callback, status="response", callback="", payload=payload)
   ws$send(toJSON(return.msg))
