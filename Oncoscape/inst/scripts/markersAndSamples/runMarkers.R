ba89fbc6e979285df864026e16947886aacad31e
pshannon-bioc <pshannon@fhcrc.org>
2015-10-26 17:24:51 -0700

    generalized module test, adding testManager and datasets tests

--- Oncoscape/inst/scripts/markersAndSamples/runMarkers.R
+++ Oncoscape/inst/scripts/markersAndSamples/runMarkers.R
@@ -18,11 +18,6 @@ printf("current.datasets: %s", current.datasets)
 
 library(OncoDev14)
 scriptDir <- "markersAndSamples"
-#userID <- "autotest.20@nowhere.org"
-#current.datasets <- "DEMOdz;UWlung"
-#current.datasets <- "DEMOdz;DEMOdz;DEMOdz"
-#current.datasets <- "DEMOdz;TCGAgbm"
-#current.datasets <- "DEMOdz;TCGAgbm;TCGAbrain"
 port <- 7575
 onco <- OncoDev14(port=port, scriptDir=scriptDir, userID=userID, datasetNames=current.datasets)
 if(Sys.info()[["nodename"]] != "lopez"){
