c1f4956e03c20f3665ae70469c2967eb0cc75246
Jenny Zhang <canaantt@gmail.com>
2015-11-19 19:56:07 -0800

    updated the instruction hidden div for pca, plsr, oncoprint and survival tab

---
+++ Oncoscape/inst/scripts/pca/runPcaApp.R
@@ -0,0 +1,10 @@
+library(OncoDev14)
+scriptDir <- "pca"
+userID <- "autoTest@nowhere.org"
+#current.datasets <- "DEMOdz;TCGAgbm"
+current.datasets <- "DEMOdz"
+port <- 7501
+onco <- OncoDev14(port=port, scriptDir=scriptDir, userID=userID, datasetNames=current.datasets)
+if(Sys.info()[["nodename"]] != "lopez") 
+   browseURL(sprintf("http://localhost:%d", port))
+run(onco)
