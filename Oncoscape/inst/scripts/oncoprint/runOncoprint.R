d147d90ab376fff30f98c1cadbd2189794b41be4
Jenny Zhang <canaantt@gmail.com>
2015-11-30 19:23:30 -0800

    still setting up

--- Oncoscape/inst/scripts/oncoprint/runOncoprint.R
+++ Oncoscape/inst/scripts/oncoprint/runOncoprint.R
@@ -18,7 +18,7 @@ printf("current.datasets: %s", current.datasets)
 
 library(OncoDev14)
 scriptDir <- "oncoprint"
-port <- 7585
+port <- 7595
 onco <- OncoDev14(port=port, scriptDir=scriptDir, userID=userID, datasetNames=current.datasets)
 if(Sys.info()[["nodename"]] != "lopez"){
   browseURL(sprintf("http://localhost:%d", port))
