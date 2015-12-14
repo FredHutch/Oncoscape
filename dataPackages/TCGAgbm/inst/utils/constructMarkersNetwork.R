2619f03f1eca6da315b478eeb36566388ac8d2eb
pshannon-bioc <pshannon@fhcrc.org>
2015-12-14 14:58:59 -0800

    standardized the current 3 constructMarkerNetwork.R scripts, updated manifestss

--- dataPackages/TCGAgbm/inst/utils/constructMarkersNetwork.R
+++ dataPackages/TCGAgbm/inst/utils/constructMarkersNetwork.R
@@ -2,42 +2,66 @@ library(NetworkMaker)
 library(SttrDataPackage)
 library(TCGAgbm)
 options(stringsAsFactors=FALSE)
-#----------------------------------------------------------------------------------------------------
+
 dz <- TCGAgbm() 
 netMaker <- NetworkMaker(dz)
-load(system.file(package="NetworkMaker", "extdata", "genesets.RData"))
-goi <- sort(unique(genesets$tcga.GBM.classifiers, genesets$marker.genes.545))
-gistic.scores <-c(-2, 2)
-#calculateSampleSimilarityMatrix(netMaker, genes=goi, copyNumberValues=gistic.scores)
-filename <- "MDS.SNV.CNV.tsv"
-usePrecalculatedSampleSimilarityMatrix(netMaker, filename)
+#----------------------------------------------------------------------------------------------------
+create.and.display <- function()
+{
+   load(system.file(package="NetworkMaker", "extdata", "genesets.RData"))
+   goi <- sort(unique(genesets$tcga.GBM.classifiers, genesets$marker.genes.545))
+   gistic.scores <-c(-2, 2)
+   #calculateSampleSimilarityMatrix(netMaker, genes=goi, copyNumberValues=gistic.scores)
+   filename <- "MDS.SNV.CNV.tsv"
+   usePrecalculatedSampleSimilarityMatrix(netMaker, filename)
 
-g <- getSamplesGraph(netMaker)
-rcy <- RCyjs(portRange=6047:6100, quiet=TRUE, graph=g, hideEdges=TRUE)
-httpSetStyle(rcy, "style.js")
-tbl.pos <- getSimilarityScreenCoordinates(netMaker, xOrigin=0, yOrigin=0, xMax=2000, yMax=5000)
-setPosition(rcy, tbl.pos)    
-fit(rcy, 100)
-g.chrom <- getChromosomeGraph(netMaker, goi)
-httpAddGraph(rcy, g.chrom)
-httpSetStyle(rcy, "style.js")
-tbl.pos <- getChromosomeScreenCoordinates(netMaker, xOrigin=1400, yOrigin=000, yMax=2000, chromDelta=200)
-setPosition(rcy, tbl.pos)
-fit(rcy, 100)
-g.mut <- getMutationGraph(netMaker, goi)
-httpAddGraph(rcy, g.mut)
-hideAllEdges(rcy)
-g.cn <- getCopyNumberGraph(netMaker, goi, gistic.scores)
-httpAddGraph(rcy, g.cn)
-hideAllEdges(rcy)
-showEdges(rcy, "chromosome")
-fit(rcy)
-# httpSetStyle(rcy, "style.js")   # smaller nodes, red on selection
-httpSetStyle(rcy, "../../../DEMOdz/inst/utils/style.js")
-# temporary fix, accomodating orphan genes (not mapped to chromosomes):
-# select those genes, place them below chr12, layoutSelectionInGridInferAnchor(rcy, 100, 100)
-g.markers.json <- getJSON(rcy)   # about 1M
-filename <- "../extdata/markers.json.TCGAgbm.RData"
-printf("saving as %s, %d nodes, %d edges", filename, getNodeCount(rcy), getEdgeCount(rcy))
-save(g.markers.json, file=filename)
- 
+   g <- getSamplesGraph(netMaker)
+   rcy <- RCyjs(portRange=6047:6100, quiet=TRUE, graph=g, hideEdges=TRUE)
+   httpSetStyle(rcy, "style.js")
+   tbl.pos <- getSimilarityScreenCoordinates(netMaker, xOrigin=0, yOrigin=0, xMax=6000, yMax=6000)
+   setPosition(rcy, tbl.pos)    
+   fit(rcy, 100)
+   g.chrom <- getChromosomeGraph(netMaker, goi)
+   httpAddGraph(rcy, g.chrom)
+   httpSetStyle(rcy, "style.js")
+   tbl.pos <- getChromosomeScreenCoordinates(netMaker, xOrigin=3400, yOrigin=0, yMax=3000, chromDelta=200)
+   setPosition(rcy, tbl.pos)
+   fit(rcy, 100)
+   g.mut <- getMutationGraph(netMaker, goi)
+   httpAddGraph(rcy, g.mut)
+   hideAllEdges(rcy)
+   g.cn <- getCopyNumberGraph(netMaker, goi, gistic.scores)
+   httpAddGraph(rcy, g.cn)
+   hideAllEdges(rcy)
+   showEdges(rcy, "chromosome")
+   fit(rcy)
+   httpSetStyle(rcy, "style.js") 
+   # temporary fix, accomodating orphan genes (not mapped to chromosomes):
+
+   unpositioned.nodes <- names(which(!noa(g, "positioned")))
+   selectNodes(rcy, unpositioned.nodes)
+   layoutSelectionInGrid(rcy, x=-2000, y=3300, w=1400, h=400)
+   fit(rcy)
+
+   return(list(rcy=rcy, g=g, g.mut=g.mut, g.cn=g.cn, unpositioned=unpositioned.nodes))
+
+} # create.and.display
+#----------------------------------------------------------------------------------------------------
+saveGraph <- function(rcy)
+{
+   g.markers.json <- getJSON(rcy)   # about 1M
+   filename <- "../extdata/markers.json.RData"
+   printf("saving as %s, %d nodes, %d edges", filename, getNodeCount(rcy), getEdgeCount(rcy))
+   save(g.markers.json, file=filename)
+
+} # saveGraph
+#----------------------------------------------------------------------------------------------------
+x <- create.and.display() 
+rcy <- x$rcy
+# some genes are not yet mapped because they use obsolete symbols.  until this is fixed, look
+# for them at 0,0, move them to i.e., below chr 12, make sure they are selected, then
+# layoutSelectionInGrid(rcy, x=5800, y=2000, w=800, h=400)
+
+
+# when you are satisifed with the layout, deselect any selections, then
+#  saveGraph(rcy)
