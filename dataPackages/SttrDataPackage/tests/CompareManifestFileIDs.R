9f6e8905be000107f1e835339c3b8ed9878f7510
LisaMc <lisa.mcferrin@gmail.com>
2016-01-24 14:59:55 -0800

    Update CompareManifestFileIDs.R
    loop over datapackage names for running tests rather than calling each one explicitly.  @grettygoose - still needs to be run and tested

--- dataPackages/SttrDataPackage/tests/CompareManifestFileIDs.R
+++ dataPackages/SttrDataPackage/tests/CompareManifestFileIDs.R
@@ -1,71 +1,21 @@
 #load libraries 
-library("TCGAbrain")
-library("TCGAbrca")
-library("DEMOdz")
-library("TCGAgbm")
-library("TCGAhnsc")
-library("TCGAlgg")
-library("TCGAluad")
-library("TCGAlung")
-library("TCGAlusc")
-library("TCGAprad")
-library("TCGAcoadread")
+
+dataPackageNames <- c("TCGAbrain", "TCGAbrca", "DEMOdz", "TCGAgbm", "TCGAhnsc", "TCGAlgg", "TCGAluad", "TCGAlung", "TCGAlusc", "TCGAprad", "TCGAcoadread")
+for(name in dataPackageNames)
+  library(name)
 
 #----------------------------------------------------------------------------------------------------
 printf <- function(...) print(noquote(sprintf(...)))
 #----------------------------------------------------------------------------------------------------
-# construct these objects for subsequent examination and testing
-
-if (!exists("brain"))
-  brain <- TCGAbrain()
-
-if (!exists("brca"))
-  brca <- TCGAbrca()
-
-if (!exists("ddz"))
-  ddz <- DEMOdz()
-
-if (!exists("gbm"))
-  gbm <- TCGAgbm()
-
-if (!exists("hnsc"))
-  hnsc <- TCGAhnsc()
-
-if (!exists("lgg"))
-  lgg <- TCGAlgg()
-
-if (!exists("luad"))
-  luad <- TCGAluad()
-
-if (!exists("lung"))
-  lung <- TCGAlung()
-
-if (!exists("lusc"))
-  lusc <- TCGAlusc()
-
-if (!exists("prad"))
-  prad <- TCGAprad()
-
-if (!exists("coadread"))
-  coadread <- TCGAcoadread()   
-  
 #----------------------------------------------------------------------------------------------------
 runTests <- function()
 {
     category.types<-c("mutations", "copy number", "mRNA expression", "protein abundance", "methylation") 
-    for(category.name in category.types){
-  
-       	exploreExpressionData(ddz, "DEMOdz", category.name)
-    	exploreExpressionData(gbm, "TCGAgbm", category.name)
-    	exploreExpressionData(brain, "TCGAbrain", category.name)
-    	exploreExpressionData(brca, "TCGAbrca", category.name)
-    	exploreExpressionData(hnsc, "TCGAhnsc", category.name)
-    	exploreExpressionData(lgg, "TCGAlgg", category.name)
-    	exploreExpressionData(luad, "TCGAluad", category.name)
-    	exploreExpressionData(lung, "TCGAlung", category.name)
-    	exploreExpressionData(lusc, "TCGAlusc", category.name)
-    	exploreExpressionData(prad, "TCGAprad", category.name)
-    	exploreExpressionData(coadread, "TCGAcoadread", category.name)
+    for(dpName in dataPackageNames){
+      eval(parse(text=sprintf("dp <- %s()",dpName)))
+      for(category.name in category.types){
+       	exploreExpressionData(dp, dpName, category.name)
+    	}
     }  
 } # runTests
 #----------------------------------------------------------------------------------------------------
