9f1d67d9161fab72ce7682d56dc22ae715fd4b4a
Michael Zager <michael@zager.co>
2016-01-26 16:51:08 -0800

    Removed OS Client V2 From Repo

--- oncoscape/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('oncoscape', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
