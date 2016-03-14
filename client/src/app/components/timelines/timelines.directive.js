c88e29282a15595c7cf1ea971137f40fff7d31c8
Michael Zager <michael@zager.co>
2016-03-13 19:18:04 -0700

    Timelines

--- client/src/app/components/timelines/timelines.directive.js
+++ client/src/app/components/timelines/timelines.directive.js
@@ -55,6 +55,7 @@
                 var drawFeatures = function(patients, feature) {
 
                     var rowHeight = Math.floor(svgHeight / patients.length);
+                    if (rowHeight<0) rowHeight = 2;
                     var thirdWidth = Math.floor(svgWidth / 3);
 
                     // Define Feature Scale
@@ -64,7 +65,7 @@
                             return patient.calcEvents[featureIndex].value
                         }))])
                         .range([0, thirdWidth]);
-console.log(featureIndex);
+
                     // Features
                     var svgFeatures = svg.selectAll("rect.feature")
                         .data(patients);
@@ -104,7 +105,9 @@ console.log(featureIndex);
 
 
                     var rowHeight = Math.floor(svgHeight / patients.length);
+                    if (rowHeight<0) rowHeight = 2;
                     var thirdWidth = Math.floor(svgWidth / 3);
+
 
                     // Define Timeline Scale
                     var timelineMin = patients
@@ -125,53 +128,113 @@ console.log(featureIndex);
                     var timelineScale = d3.scale.linear()
                         .domain([timelineMin, timelineMax])
                         .range([0, thirdWidth * 2]);
+//debugger;
 
-var color = function(d){
-    var status = d.name;
-    var rv = 
-        (status==="Birth") ?  "#004358" : 
-        (status==="Diagnosis") ? "#1F8A70" :
-        (status==="Pathology") ? "#BEDB39" :
-        (status==="Progression") ? "#FFE11A" :
-        (status==="Absent") ? "#FD7400" :
-        (status==="Status") ? "#000000" :
-        "#00FFFF";
-        return rv;
-};
-                    
-
-                    var svgTimelines = svg.selectAll("rect.patient")
-                        .data(patients)
-                        .enter()
-                        .append("g")
-                        .classed("patient", true)
-                        .attr("transform", function(d, i) {
-                            return "translate(0," + i * rowHeight + ")";
-                        });
-
+/*
+patients.forEach(function(p){
+  p.dateEvents.forEach(function(d){
+    var v = Math.round(timelineScale(d.start)) + thirdWidth;
+console.log(v);
+  })
+})
+*/
+                    var color = function(d){
+                        var status = d.name;
+                        var rv = 
+                            (status==="Birth") ?  "#004358" : 
+                            (status==="Diagnosis") ? "#1F8A70" :
+                            (status==="Pathology") ? "#BEDB39" :
+                            (status==="Progression") ? "#FFE11A" :
+                            (status==="Absent") ? "#FD7400" :
+                            (status==="Status") ? "#59a5fb" :
+                            "#00FFFF";
+                            return rv;
+                    };
 
 
-                    svgTimelines.selectAll("rect.timeline")
-                        .data(function(d, i) {
-                            return d.dateEvents;
-                        })
-                        .enter()
-                        .append("rect")
-                        .style({
-                            "fill": color
-                        })
-                        .attr({
-                            "class": "timeline",
-                            "x": function(d) {
-                                return timelineScale(d.start) + thirdWidth
-                            },
-                            "width": "3",
-                            "height": function() {
-                                return rowHeight - 1;
-                            }
-                        })
+var rows = svg.selectAll("g")
+    .data( patients);
 
-                }
+var cells = rows.selectAll("rect")
+    .data( function(d, i){ return d.dateEvents;  });
+
+
+     cells
+        .transition()
+        .duration(300)
+        .style({'opacity':1, 'fill':color})
+        .attr({
+            "x": function(d) { return Math.round(timelineScale(d.start)) + thirdWidth;},
+            "width": 3,
+        });
+
+    // Row Update
+    cells
+        .enter()
+        .append("rect")
+        .style({'opacity': 0.0, 'fill':color})
+        .attr('class', 'enter')
+        .attr({
+            'x': 0,
+            'height': (rowHeight - 1)
+        })
+        .transition()
+        .duration(300)
+        .attr({
+            'x': function(d) { return Math.round(timelineScale(d.start)) + thirdWidth;},
+        });
+
+   
+
+    cells
+        .exit()
+        .attr('class', 'exit')
+        .transition()
+        
+        .duration(300)
+        .style('opacity', 0.0)
+        .attr({'x':0})
+        .remove();
+
+    // Row Enter
+    var cells_in_new_rows = rows
+        .enter()
+        .append("g")
+        .attr("transform", function(d, i) {return "translate(0," + i * rowHeight + ")"; })
+        .selectAll('rect')
+        .data( function(d, i){ return d.dateEvents; } );
+
+    cells_in_new_rows.enter().append('rect')
+        .style('opacity', 0.0)
+        .attr('class', 'enter')
+        .attr({
+            'x': thirdWidth,
+            'width': 3,
+            'height': (rowHeight - 1)
+        })
+        .style({'opacity': 0.0, 'fill':color})
+        .transition()
+        .duration(500)
+        .style('opacity', 1.0)
+        .attr({
+            'x': function(d) { return timelineScale(d.start) + thirdWidth },
+            'width':function(d){
+
+                var rv = (d.end===null) ? 3 : (timelineScale(d.end) - timelineScale(d.start));
+                console.log(rv);
+                return rv;
+            }
+        });
+
+    // Row Exit
+    rows.exit()
+      .attr('class', 'exit')
+      .transition()
+      .delay(200)
+      .duration(500)
+      .style('opacity', 0.0)
+      .remove();
+                 }
 
 
 
@@ -212,23 +275,23 @@ var color = function(d){
                             .map(function(d) {
                                 return {
                                     "start": m(d.date, mf).unix() * 1000,
-                                    "end": m(d.date, mf).endOf('day').unix() * 1000,
-                                    name: d.name
+                                    "end": null,
+                                    "name": d.name
                                 };
                             })
 
-                        /*
+                        
                         var endDates = p.dateEvents
                             .filter(function(d) { return d.eventOrder==='end' })
-                            .map(function(d){ return {"ending_time":m(d.date, mf).unix()*1000, "id":d.eventID}; });
+                            .map(function(d){ return {"end":m(d.date, mf).unix()*1000, "id":d.eventID}; });
                         var startDates = p.dateEvents
                             .filter(function(d) { return d.eventOrder==='start' })
-                            .map(function(d){ return {"starting_time":m(d.date, mf).unix()*1000, "taskName":p.id, "status":d.name, "id":d.eventID}; })
+                            .map(function(d){ return {"start":m(d.date, mf).unix()*1000,"name":d.name, "id":d.eventID}; })
                             .forEach(function(sd){
-                                sd.ending_time = endDates.filter( function(ed) { return ed.id===sd.id })[0].ending_time;
+                                sd.end = endDates.filter( function(ed) { return ed.id===sd.id })[0].end;
                             });
-                        */
-
+                        
+                        Array.prototype.push.apply(p.dateEvents, startDates);
                     }
 
 
