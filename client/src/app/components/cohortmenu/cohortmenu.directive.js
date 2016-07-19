abdc0f3bf40c61bdbdf0e3271b9e553521ab9186
Michael Zager <michael@zager.co>
2016-07-19 16:01:48 -0700

    cleanup of unused components

---
+++ client/src/app/components/cohortmenu/cohortmenu.directive.js
@@ -0,0 +1,48 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('oncoscape')
+        .directive('osCohortMenu', cohortMenu);
+
+    /** @ngInject */
+    function cohortMenu() {
+
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'app/components/cohortmenu/cohortmenu.html',
+            controller: CohortMenuController,
+            controllerAs: 'vm',
+            scope:{
+                datasource: '@',
+                change: '&'
+            },
+            bindToController: true
+        };
+
+        return directive;
+
+        /** @ngInject */
+        function CohortMenuController(osApi, $state) {
+
+      
+            var mouseOver = function(){
+                angular.element(".tool-menu")
+                    .removeClass("tray-collapsed-left");
+            }
+
+            var mouseOut = function(){
+                angular.element(".tool-menu")
+                    .addClass("tray-collapsed-left");
+            }
+
+            var elTray = angular.element(".tool-menu");
+                elTray
+                    .bind("mouseover", mouseOver)
+                    .bind("mouseout", mouseOut);
+            
+            
+        }
+    }
+
+})();
