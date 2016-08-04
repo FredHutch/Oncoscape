c6fe2075d8d1f43ec2d98f7cb3be1b1f6a784eac
Michael Zager <michael@zager.co>
2016-08-04 16:11:08 -0700

    Lisa Data Changes + Kong Init

--- kong/node_modules/co-mongodb/index.js
+++
@@ -1,7 +0,0 @@
-exports.client = require('./lib/client');
-exports.db = require('./lib/db');
-exports.collection = require('./lib/collection');
-exports.admin = require('./lib/admin');
-exports.cursor = require('./lib/cursor');
-exports.grid = require('./lib/grid');
-exports.gridstore = require('./lib/gridstore');
