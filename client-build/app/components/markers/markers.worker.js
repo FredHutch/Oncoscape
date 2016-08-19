4a1022d66bb596013e73ee8adb4e3c1d30068929
Michael Zager <michael@zager.co>
2016-08-19 07:35:42 -0700

    Update markers.worker.js

--- client-build/app/components/markers/markers.worker.js
+++ client-build/app/components/markers/markers.worker.js
@@ -19,7 +19,7 @@
                 resolve(data);
                 return;
             }
-            var query = "http://localhost:80/api/" + object.table;
+            var query = "/api/" + object.table;
             if (object.query) query += "/" + encodeURIComponent(JSON.stringify(object.query));
             load(query, function(response) {
                 resolve(format(JSON.parse(response.responseText)));
