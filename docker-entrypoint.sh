ce2a51765dde25ed18c244dd44a3897fd45ac059
Michael Zager <michael@zager.co>
2016-09-25 05:59:52 -0700

    env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 htpasswd -b -c /home/sttrweb/Oncoscape/.htpasswd $HT_USERNAME $HT_PASSWORD
+envsubst < /home/sttrweb/Oncoscape/docker-kong.template > /home/sttrweb/Oncoscape/docker-kong.conf
 /usr/bin/supervisord -n -c /home/sttrweb/Oncoscape/docker-supervisord.conf
