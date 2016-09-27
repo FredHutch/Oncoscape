d83a9e489e223d62c208e9e84ee167317821db2f
Michael Zager <michael@zager.co>
2016-09-27 05:27:40 -0700

    Adjust Ports

--- client/src/app/components/colorpanel/colorpanel.directive.js
+++ client/src/app/components/colorpanel/colorpanel.directive.js
@@ -168,7 +168,7 @@ debugger;
                             color: "#a9a10a"
                         }];
 
-                        var scale = d3.scale.quantize()
+                        var scale = d3.scaleQuantize()
                             .domain([data.min, data.max])
                             .range(values.map(function(f) {
                                 return f.color;
