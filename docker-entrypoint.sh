e71f705fce403fe12a28cc212e58ae258f3ff467
Michael Zager <michael@zager.co>
2016-09-26 18:52:05 -0700

    . . .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 htpasswd -b -c /home/sttrweb/Oncoscape/.htpasswd $HT_USERNAME $HT_PASSWORD
-export kong_container_ip=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
+#export kong_container_ip=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 envsubst < /home/sttrweb/Oncoscape/docker-kong.template > /home/sttrweb/Oncoscape/docker-kong.conf
 /usr/bin/supervisord -n -c /home/sttrweb/Oncoscape/docker-supervisord.conf
