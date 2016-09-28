d24c397b9e15551bbeedbeee86c113ad8239f659
Michael Zager <michael@zager.co>
2016-09-28 15:27:55 -0700

    . . . .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,8 @@ htpasswd -b -c /home/sttrweb/Oncoscape/.htpasswd $HT_USERNAME $HT_PASSWORD
 # Export Container Id - Conditional Logic Required Due to Issue With DockerCloud Overlay Network
 export kong_container_ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)";
 case $HOSTNAME in
-  "oncoscape-dev-1") export kong_container_ip="172.17.12.120";;
-  "oncoscape-dev-2") export kong_container_ip="172.17.12.29";;
+  "oncoscape-dev-1") export kong_container_ip="172.17.12.29";;
+  "oncoscape-dev-2") export kong_container_ip="172.17.12.120";;
 esac
 
 # Inject Environment Vars Into Config
