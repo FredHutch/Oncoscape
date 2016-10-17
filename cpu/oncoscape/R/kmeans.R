6a3601ac2f7389f7eadc0ce6afe1d980528c772a
Michael Zager <michael@zager.co>
2016-10-17 10:39:05 -0700

    update ocpu

--- cpu/oncoscape/R/kmeans.R
+++ cpu/oncoscape/R/kmeans.R
@@ -1,16 +1,19 @@
-
 kmeans = function(x, centers=2, algorithm=c("Hartigan-Wong", "Lloyd", "Forgy","MacQueen")) {
         result = stats::kmeans(x, centers, algorithm = algorithm)
         jsonlite::toJSON(result[c("cluster","centers")])
 }
 
-
-
 heatmap = function(data){
-        x <- d3heatmap(data, scale = "column", colors = "Blues")
-        htmlwidgets:::toJSON(x)
-}
 
-oheatmap = function(data){
-        pheatmap::pheatmap(data, kmeans_k = 3)
+        # Generate Chart
+        chart <- d3heatmap::d3heatmap(data, scale = "column", colors = "Blues")$x
+
+        # Strip Overhead
+        chart$image <- NULL
+        chart$options <- NULL
+        chart$theme <- NULL
+
+        # Transmit JSON
+        htmlwidgets:::toJSON(chart)
+
 }
